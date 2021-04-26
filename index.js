e267c4d232f13c0e2a31312906d380a3de0882fd
Luis Hernandez <datboiluiskrrt@gmail.com>
2021-04-25 21:35:43 -0500

    How to join two arrays and how to use spread operator

--- index.js
+++ index.js
@@ -15,5 +15,27 @@ hijos
 // How to add elements to the BEGINNING of the array
 hijos.unshift("Kiri Kiri")
 hijos
-hijos.unshift("Miguelito", "Simba")
+
+// How to remove the LAST item of an array
+hijos.pop()
 hijos
+
+// remove item from BEGINNING of array
+hijos.shift()
+hijos
+
+// How to join two or more arrays
+
+const ay = [1, 2]
+const b = [3,4]
+const c = ay.concat(b)
+ay
+b
+c
+
+//or we can use spread operator ...
+const d = [1,2]
+const e = [3,4]
+const f = [ ...d, ...e]
+f
+
