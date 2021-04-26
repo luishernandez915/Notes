136a7559a89fdca02d8bff54650142cbdf112d0a
Luis Hernandez <datboiluiskrrt@gmail.com>
2021-04-25 21:26:54 -0500

    How to add elements to an array

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ const a = [1, 2, 3]
 /* By using the length = we can either add or delete the length of the array  */
 a.length = 7
 a
+
+// How to add elements to the END of the array
+let hijos = ["luis", "Fer", "faby"];
+hijos
+hijos.push("pelusita")
+hijos
+
+// How to add elements to the BEGINNING of the array
+hijos.unshift("Kiri Kiri")
+hijos
+hijos.unshift("Miguelito", "Simba")
+hijos
